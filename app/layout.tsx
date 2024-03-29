6a6d3af178b7148af29ac622df2708c9c0cf1f75
moosa <moosaabdullahi45@gmail.com>
2024-03-30 01:54:34 +0200

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,10 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Stargate Academy",
+  title: "Stargate Academy - Nurturing Academic and Spiritual Excellence",
   description: "Stargate Academy: Nurturing Academic and Spiritual Excellence since 2008. Discover our comprehensive curriculum blending Nigerian, British, and Islamic studies. Join a community where education goes beyond the classroom, empowering every student to shine bright. Explore now",
+  keywords: "Stargate Academy, academic excellence, spiritual development, comprehensive curriculum, Nigerian curriculum, British curriculum, Islamic studies",
+  authors: [{name:"Stargate Academy", url:"https://www.stargateexcellenceacademy.com.ng/"}]
 };
 
 export default function RootLayout({
