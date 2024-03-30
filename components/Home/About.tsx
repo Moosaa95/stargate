fe158fd06a0e15858ac01e9de55f0858f540e81d
moosa <moosaabdullahi45@gmail.com>
2024-03-30 02:28:37 +0200

    new

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -10,38 +10,37 @@ interface AboutProps {
 }
 const About = ({title, text_one, text_two}: AboutProps) => {
   return (
-    <section className='w-full bg-white py-16 px-4'>
-        <div className="max-w-[1240px] gap-10 mx-auto grid md:grid-cols-5">
-        <div className='col-span-3'>
-            <motion.div
-                variants={fadeIn("up", 0.1)}
-                initial="hidden"
-                whileInView={"show"}
-                viewport={{once:false, amount:0.7}}
-                 className="flex flex-col justify-between">
-                
-                <h1 className='text-gray-90 md:text-4xl sm:text-3xl text-2xl font-bold py-2'>{title}</h1>
-                <p className="text-lg lg:text-xl text-gray-700 mb-6">
-                    {/* Stargate Academy is a beacon of academic excellence committed to
-                    nurturing young minds and fostering a culture of lifelong learning and
-                    personal growth. */}
-                    {text_one}
-               </p>
-               <p className="text-lg lg:text-xl text-gray-700 mb-6">
-                {/* Founded in 2008, Stargate Academy has a rich history of providing a
-                comprehensive education that blends rigorous academic standards with
-                spiritual and moral development, preparing students not just for exams
-                but for life. */}
-                {text_two}
-                </p>
-                {/* <button className='bg-gray-90 w-[200px] rounded-md font-medium my-6 mx-auto py-3 md:mx-0 text-white hover:font-bold'>Learn More</button> */}
-            </motion.div>
-            </div>
-            <div className='col-span-2 rounded-md'>
-            <Image src="/assets/images/stargate4.jpg" alt='stargate building' width={600} height={300} className='mx-auto my-4 rounded-md'/>
-            </div>
+    <section className="w-full bg-white py-16 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2 gap-10">
+        <div className="col-span-1">
+          <motion.div
+            variants={fadeIn("up", 0.1)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0.7 }}
+            className="flex flex-col justify-between h-full"
+          >
+            <h1 className="text-gray-90 md:text-4xl sm:text-3xl text-2xl font-bold py-2">
+              {title}
+            </h1>
+            <p className="text-lg lg:text-xl text-gray-700 mb-6">
+              {text_one}
+            </p>
+            <p className="text-lg lg:text-xl text-gray-700 mb-6">
+              {text_two}
+            </p>
+          </motion.div>
         </div>
-
+        <div className="col-span-1 rounded-md overflow-hidden">
+          <Image
+            src="/assets/images/stargate4.jpg"
+            alt="stargate building"
+            width={400}
+            height={200}
+            className="mx-auto my-4 rounded-md"
+          />
+        </div>
+      </div>
     </section>
   );
 };
