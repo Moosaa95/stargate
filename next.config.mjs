72f25fd589fcc140d8960c1e15c3a8fbc8cc32d2
moosa <moosaabdullahi45@gmail.com>
2024-06-27 10:26:45 +0200

    add reverse

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   async rewrites() {
     return [
       {
+        source: "/dossier",
+        destination: "https://www.stargateexcellenceacademy.com.ng/dossier",
+      },
+      {
         source: "/dossier/:path*",
         destination:
           "https://www.stargateexcellenceacademy.com.ng/dossier/:path*",
